58b7ba961fbc5e6215ba603ebb03e9fe9d160be2
Vili Hätönen <vili.hatonen@helsinki.fi>
2016-03-19 23:01:52 +0200

    initial commit gdxSTKTdemo

---
+++ desktop/src/fi/ymcafinland/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.ymcafinland.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.ymcafinland.demo.SelviytyjanPurjeet;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SelviytyjanPurjeet(), config);
+	}
+}
