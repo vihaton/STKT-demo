9702bdacc3e300a055d72743b1d2e85d552d1231
Syaniidikauppias <jouni.winter@gmail.com>
2016-03-22 12:31:56 +0200

    Eka testi

---
+++ core/test/fi/ymcafinland/demo/tests/logiikkaTest/EkaTest.java
@@ -0,0 +1,18 @@
+package fi.ymcafinland.demo.tests.logiikkaTest;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EkaTest {
+
+    @Test
+    public void passingTest() {
+        assertTrue(true);
+    }
+
+//    @Test
+//    @Ignore
+//    public void ignoreTest() {
+//    }
+}
