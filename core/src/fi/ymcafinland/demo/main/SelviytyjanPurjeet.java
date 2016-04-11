9ad6a8bc771b968f81f4a852450707b9fe50dfc2
Syaniidikauppias <jouni.winter@gmail.com>
2016-04-11 22:58:13 +0300

    Infoscreeni 1.0

--- core/src/fi/ymcafinland/demo/main/SelviytyjanPurjeet.java
+++ core/src/fi/ymcafinland/demo/main/SelviytyjanPurjeet.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import fi.ymcafinland.demo.screens.InfoScreen;
 import fi.ymcafinland.demo.screens.PlayScreen;
 import fi.ymcafinland.demo.logiikka.Verkko;
 import fi.ymcafinland.demo.screens.QuestionScreen;
@@ -27,7 +28,8 @@ public class SelviytyjanPurjeet extends Game {
 		verkko = new Verkko(taustakuva.getWidth(), taustakuva.getHeight());
 
 		this.playscreen = new PlayScreen(this, taustakuva, verkko.getSolmut().get(0));
-		setScreen(playscreen);
+
+		setScreen(new InfoScreen(this));
 
 	}
 	@Override
---
+++ core/src/fi/ymcafinland/demo/screens/InfoScreen.java
@@ -0,0 +1,89 @@
+package fi.ymcafinland.demo.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import fi.ymcafinland.demo.main.SelviytyjanPurjeet;
+
+/**
+ * Created by jwinter on 11.4.2016.
+ */
+public class InfoScreen implements Screen {
+    protected SpriteBatch batch;
+
+    private final SelviytyjanPurjeet sp;
+    private FitViewport viewport;
+    private OrthographicCamera camera;
+    private BitmapFont fontti;
+    private Sprite tausta;
+
+    public InfoScreen(SelviytyjanPurjeet sp) {
+        this.sp = sp;
+        this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        this.viewport = new FitViewport(sp.V_WIDTH, sp.V_HEIGHT, camera);
+        this.fontti = new BitmapFont(Gdx.files.internal("font/fontti.fnt"), Gdx.files.internal("font/fontti.png"), false);
+        this.tausta = new Sprite(new Texture("sails02.png"));
+
+
+        camera.setToOrtho(false, sp.V_WIDTH, sp.V_HEIGHT);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        float rgbJakaja = 255f;
+        Gdx.gl.glClearColor(0, 0, 139 / rgbJakaja, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+
+        tausta.draw(batch);
+        fontti.draw(batch, "Selviytyjän purjeet", 25 , 900);
+        batch.end();
+
+        if (Gdx.input.isTouched()) {
+            sp.resetPlayScreen();
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
