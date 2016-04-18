1a4636ebf2d2010c9f314e25cb608a5e061c259f
xvixvi <vili.hatonen@ymca.fi>
2016-04-18 14:27:56 +0300

    slider pois väittämältä, ui asiat piirtäjiin!

--- core/src/fi/ymcafinland/demo/logiikka/Vaittama.java
+++ core/src/fi/ymcafinland/demo/logiikka/Vaittama.java
@@ -13,18 +13,10 @@ public class Vaittama {
     private String teksti;
     private String solmunID;
     private float arvo;
-    private Slider slider;
 
     public Vaittama(String txt, String solmunID) {
         this.teksti = txt;
         this.solmunID = solmunID;
-
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("slider/slider.pack"));
-        Skin skin = new Skin();
-        skin.addRegions(atlas);
-        Slider.SliderStyle style = new Slider.SliderStyle(skin.getDrawable("sliderbackground"), skin.getDrawable("sliderknob"));
-        this.slider = new Slider(-5, 5, .2f, false, new Slider.SliderStyle(skin.getDrawable("sliderbackground"), skin.getDrawable("sliderknob")));
-        slider.setAnimateDuration(0.3f);
     }
 
     public String getTeksti() {
