cbe37dafeaa275f7c1b3ce83cdbd117944b20567
xvixvi <vili.hatonen@ymca.fi>
2016-04-28 18:55:03 +0300

    Väittämänpiirtäjän refaktorointi ja siivous (esim scrollpanee ei luoda jokaisen väittämän kohdalla uudestaan...)

--- core/src/fi/ymcafinland/demo/piirtajat/VaittamanPiirtaja.java
+++ core/src/fi/ymcafinland/demo/piirtajat/VaittamanPiirtaja.java
@@ -36,25 +36,34 @@ public class VaittamanPiirtaja {
         this.rootTable = rootTable;
         this.sliderit = new ArrayList<>();
         skin = masterSkin;
+        luoScrollPane();
+    }
+
+    private void luoScrollPane() {
+        pane = new ScrollPane(rootTable.top());
+        pane.setBounds(0, 0, SelviytyjanPurjeet.V_WIDTH, SelviytyjanPurjeet.V_HEIGHT / 1.35f);
+        pane.setTouchable(Touchable.enabled);
+        pane.validate();
+
+        stage.addActor(pane);
+
+        rootTable.padBottom(Gdx.graphics.getHeight() / 6);
     }
 
     public void renderoi(SpriteBatch batch, float delta) {
-
         batch.begin();
         for (Slider s : sliderit) {
-                s.act(delta);
+            s.act(delta);
         }
 
         pane.act(delta);
         stage.draw();
         batch.end();
-
     }
 
     public void paivitaVaittamat(ArrayList<Vaittama> solmunVaittamat) {
         rootTable.reset();
 
-        //ToDo jos sliderin arvoa ei muuteta, pelaajan selviytymis attribuutti ei pitäisi muuttua.
         for (final Vaittama nykyinenVaittama : solmunVaittamat) {
             Table vaittamanTaulukko = new Table();
             Label otsikko = new Label(nykyinenVaittama.getTeksti(), skin, "vaittamatyyli");
@@ -66,21 +75,8 @@ public class VaittamanPiirtaja {
             final Slider slider = new Slider(0.5f, 1.5f, .1f, false, skin.get("sliderStyle", Slider.SliderStyle.class));
             slider.setAnimateDuration(0.1f);
             slider.setValue(nykyinenVaittama.getArvo());
-            slider.addListener(new ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("UITest", "slider: " + slider.getValue());
+            luoKuuntelijat(nykyinenVaittama, slider);
 
-                    nykyinenVaittama.setArvo(slider.getValue());
-
-                }
-            });
-            slider.addListener(new InputListener() {
-                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                    event.stop();
-                    return false;
-                }
-            });
             sliderit.add(slider);
 
             vaittamanTaulukko.add(otsikko).width(slider.getWidth() * 3);
@@ -89,16 +85,23 @@ public class VaittamanPiirtaja {
 
             rootTable.add(vaittamanTaulukko);
             rootTable.row();
-            //ToDo Oletettavasti jokaisella solmulla on tarpeeksi kysymyksiä ettei näkymä näytä vammaselta scrollpanen sisällä, mutta ei välttämättä vielä demossa. Tehdään jokin purkkaviritelmä? vrt. esim C3 ja C3
-
-            pane = new ScrollPane(rootTable.bottom());
-            pane.setBounds(0, 0, SelviytyjanPurjeet.V_WIDTH, SelviytyjanPurjeet.V_HEIGHT / 1.35f);
-            pane.setTouchable(Touchable.enabled);
-            pane.layout();
-
-            stage.addActor(pane);
         }
+    }
 
-        rootTable.padBottom(Gdx.graphics.getHeight() / 6);
+    private void luoKuuntelijat(final Vaittama nykyinenVaittama, final Slider slider) {
+        slider.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.log("UITest", "slider: " + slider.getValue());
+
+                nykyinenVaittama.setArvo(slider.getValue());
+            }
+        });
+        slider.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                event.stop();
+                return false;
+            }
+        });
     }
 }
