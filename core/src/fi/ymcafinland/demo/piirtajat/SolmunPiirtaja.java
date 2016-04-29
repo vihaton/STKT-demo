3f5a5994b784855fe67058dc54600c9c7ceb87a3
xvixvi <vili.hatonen@ymca.fi>
2016-04-29 16:05:58 +0300

    Merge branch 'master' of /home/xvixvi/asprojects/STKT-demo with conflicts.

--- core/src/fi/ymcafinland/demo/piirtajat/SolmunPiirtaja.java
+++ core/src/fi/ymcafinland/demo/piirtajat/SolmunPiirtaja.java
@@ -6,13 +6,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 
 import java.util.ArrayList;
 
 import fi.ymcafinland.demo.logiikka.*;
+import fi.ymcafinland.demo.screens.PlayScreen;
 
 /**
  * Created by xvixvi on 1.4.2016.
@@ -29,44 +41,26 @@ public class SolmunPiirtaja {
     private Label sisalto;
     private float pallonLeveys;
     private float pallonKorkeus;
-    private ArrayList<Table> solmuTaulukot;
+    public Stage pallostage;
+    private Table palloTable;
 
-    public SolmunPiirtaja(Verkko verkko, Skin masterSkin) {
+    public SolmunPiirtaja(Verkko verkko) {
         solmut = verkko.getSolmut();
-        skin = masterSkin;
         pallonKuva = new Texture("emptynode.png");
         pallonLeveys = pallonKuva.getWidth();
         pallonKorkeus = pallonKuva.getHeight();
 
+        fontti = new BitmapFont(Gdx.files.internal("font/fontti.fnt"), Gdx.files.internal("font/fontti.png"), false); //must be set true to be flipped
         tekstitaulukko = new Table();
+        skin = new Skin();
+        labelStyle = new Label.LabelStyle(fontti, fontti.getColor());
+        Label.LabelStyle sisaltotyyli = new Label.LabelStyle(new BitmapFont(), Color.BLACK);
+        skin.add("otsikko", labelStyle);
+        skin.add("sisalto", sisaltotyyli);
         otsikko = new Label("ymca", skin, "otsikko");
         sisalto = new Label("ymca", skin, "sisalto");
-
-        solmuTaulukot = new ArrayList<>();
-        generoiSolmutaulukot(verkko);
-    }
-
-    private void generoiSolmutaulukot(Verkko verkko) {
-        for (Solmu s : verkko.getSolmut()) {
-            float x = s.getXKoordinaatti();
-            float y = s.getYKoordinaatti();
-
-            Label otsikko = new Label(s.getOtsikko(), skin, "otsikko");
-            otsikko.setFontScale(0.7f);
-            Label sisalto = new Label(s.getSisalto(), skin, "sisalto");
-            sisalto.setFontScale(2);
-
-            Table tekstit = new Table();
-            tekstit.setPosition(x, y);
-
-            if (otsikko.getText().length != 0) { //jos otsikko ei ole tyhjä
-                tekstit.add(otsikko);
-                tekstit.row();
-            }
-            tekstit.add(sisalto);
-
-            solmuTaulukot.add(tekstit);
-        }
+        pallostage = new Stage();
+        palloTable = new Table();
     }
 
     /**
@@ -75,19 +69,59 @@ public class SolmunPiirtaja {
      * @param batch              vastaa piirtämisestä.
      * @param angleToPointCamera kulma, jolla kamera on suunnattu keskipisteeseen
      */
-    public void piirra(SpriteBatch batch, float angleToPointCamera) {
-        piirraPalloJaTekstiErikseen(batch, angleToPointCamera);
+    public void piirra(SpriteBatch batch, float angleToPointCamera, PlayScreen ps) {
+        piirraPalloJaTekstiErikseen(batch, angleToPointCamera, ps);
     }
 
-    private void piirraPalloJaTekstiErikseen(SpriteBatch batch, float angleToPointCamera) {
+    private void piirraPalloJaTekstiErikseen(SpriteBatch batch, float angleToPointCamera,final PlayScreen ps) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // This cryptic line clears the screen.
 
         batch.begin();
-        for (Table t:solmuTaulukot) {
-            batch.draw(pallonKuva, t.getX() - pallonLeveys / 2, t.getY() - pallonKorkeus / 2, pallonLeveys / 2, pallonKorkeus / 2, pallonLeveys, pallonKorkeus, 1f, 1f, angleToPointCamera - 90, 0, 0, (int) pallonLeveys, (int) pallonKorkeus, false, false);
-            t.setTransform(true);
-            t.setRotation(angleToPointCamera - 90);
-            t.draw(batch, 1f);
+        for (int i = 0; i < solmut.size(); i++) {
+            final Solmu s = solmut.get(i);
+            float x = s.getXKoordinaatti();
+            float y = s.getYKoordinaatti();
+
+            batch.draw(pallonKuva, x - pallonLeveys / 2, y - pallonKorkeus / 2, pallonLeveys / 2, pallonKorkeus / 2, pallonLeveys, pallonKorkeus, 1f, 1f, angleToPointCamera - 90, 0, 0, (int) pallonLeveys, (int) pallonKorkeus, false, false);
+
+            otsikko.setText(s.getOtsikko());
+            otsikko.setFontScale(0.7f);
+            sisalto.setText(s.getSisalto());
+            sisalto.setFontScale(2);
+
+            tekstitaulukko.reset();
+            palloTable.reset();
+
+            Button button = new Button();
+            button.setPosition(x, y);
+            button.setBounds(x, y, pallonLeveys, pallonKorkeus);
+            button.setFillParent(true);
+
+
+
+            button.addListener(new ChangeListener() {
+                public void changed(ChangeEvent event, Actor actor) {
+                    Gdx.app.log("SolmunPiirtäjä", "Solmua " + s.getID() + " klikattu");
+                    ps.setSolmu(s);
+
+                }
+            });
+
+            palloTable.add(button);
+            palloTable.setPosition(x, y);
+            palloTable.setFillParent(true);
+            palloTable.setBounds(x, y, pallonLeveys, pallonKorkeus);
+
+            pallostage.addActor(palloTable);
+            if (otsikko.getText().length != 0) { //jos otsikko ei ole tyhjä
+                tekstitaulukko.add(otsikko);
+                tekstitaulukko.row();
+            }
+            tekstitaulukko.add(sisalto);
+            tekstitaulukko.setTransform(true);
+            tekstitaulukko.setRotation(angleToPointCamera - 90);
+
+            tekstitaulukko.draw(batch, 1f);
         }
         batch.end();
     }
