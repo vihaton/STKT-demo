1c651e5c4500685e62f9724fbac7bd3269334736
Syaniidikauppias <jouni.winter@gmail.com>
2016-05-25 21:27:52 +0300

    InfoButtonKasittelijan luoKuuntelija -metodin refaktorointi NO DEBUG

--- core/src/fi/ymcafinland/demo/kasittelijat/InfoButtonKasittelija.java
+++ core/src/fi/ymcafinland/demo/kasittelijat/InfoButtonKasittelija.java
@@ -47,7 +47,7 @@ public class InfoButtonKasittelija {
     private void luoInfoNapit() {
         for (Solmu s : solmut) {
             Button infoButton = new Button(skin.get("infoButtonStyle", Button.ButtonStyle.class));
-            luoKuuntelija(infoButton, s);
+            lisaaKuuntelija(infoButton, s);
 
             Table buttonTable = new Table();
 
@@ -65,76 +65,110 @@ public class InfoButtonKasittelija {
     }
 
     //todo bug: infonappulat ei toimi, luuleeko panoroinniks?
-    private void luoKuuntelija(Button button, Solmu s) {
-        if (Integer.parseInt(s.getID()) == 1) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
+    private void lisaaKuuntelija(Button button, Solmu s) {
 
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("http://www.ymca.fi");
-                    }
-                }
-            });
+        //Switch case hoitaa vertailun, luoKuuntelija metodi luo kuuntelijan nappulalle halutun nettisivun kera
+        switch (Integer.parseInt(s.getID())) {
+            case 1:
+                button.addListener(luoKuuntelija("http://www.ymca.fi"));
+                break;
+            case 2:
+                button.addListener(luoKuuntelija("http://www.mielenterveysseura.fi/fi"));
+                break;
+            case 3:
+                button.addListener(luoKuuntelija("https://www.ray.fi"));
+                break;
+            case 4:
+                button.addListener(luoKuuntelija("http://www.cameraobscura.fi/"));
+                break;
+            case 5:
+                button.addListener(luoKuuntelija("https://www.youtube.com/watch?v=9R8aSKwTEMg"));
+                break;
+            case 6:
+                button.addListener(luoKuuntelija("https://www.riemurasia.net/kuva/Typera-jaatelo/164917"));
+                break;
         }
-        if (Integer.parseInt(s.getID()) == 2) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
-
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("http://www.mielenterveysseura.fi/fi");
-                    }
-                }
-            });
-        }
-        if (Integer.parseInt(s.getID()) == 3) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
-
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("https://www.ray.fi");
-                    }
-                }
-            });
-        }
-        if (Integer.parseInt(s.getID()) == 4) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
-
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("http://www.cameraobscura.fi/");
-                    }
-                }
-            });
-        }
-        if (Integer.parseInt(s.getID()) == 5) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
-
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("https://www.youtube.com/watch?v=9R8aSKwTEMg");
-                    }
-                }
-            });
-        }
-        if (Integer.parseInt(s.getID()) == 6) {
-            button.addListener(new ChangeListener() {
-                public void changed(ChangeEvent event, Actor actor) {
-                    Gdx.app.log("IBK", "Info nappulaa painettu");
-
-                    if (!zoomedOut) {
-                        Gdx.net.openURI("https://www.riemurasia.net/kuva/Typera-jaatelo/164917");
-                    }
-                }
-            });
-        }
-
+        //Todo tänne log ehdollisuus masterswitchin kautta?
+        Gdx.app.log("IBK", "kuuntelija " + button.getClickListener() + "luotu");
+//        if (Integer.parseInt(s.getID()) == 1) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("http://www.ymca.fi");
+//                    }
+//                }
+//            });
+//        }
+//        if (Integer.parseInt(s.getID()) == 2) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("http://www.mielenterveysseura.fi/fi");
+//                    }
+//                }
+//            });
+//        }
+//        if (Integer.parseInt(s.getID()) == 3) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("https://www.ray.fi");
+//                    }
+//                }
+//            });
+//        }
+//        if (Integer.parseInt(s.getID()) == 4) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("http://www.cameraobscura.fi/");
+//                    }
+//                }
+//            });
+//        }
+//        if (Integer.parseInt(s.getID()) == 5) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("https://www.youtube.com/watch?v=9R8aSKwTEMg");
+//                    }
+//                }
+//            });
+//        }
+//        if (Integer.parseInt(s.getID()) == 6) {
+//            button.addListener(new ChangeListener() {
+//                public void changed(ChangeEvent event, Actor actor) {
+//                    Gdx.app.log("IBK", "Info nappulaa painettu");
+//
+//                    if (!zoomedOut) {
+//                        Gdx.net.openURI("https://www.riemurasia.net/kuva/Typera-jaatelo/164917");
+//                    }
+//                }
+//            });
+//        }
     }
 
+    private ChangeListener luoKuuntelija(final String linkkisivu) {
+        return new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.log("IBK", "Info nappulaa painettu");
+
+                if (!zoomedOut) {
+                    Gdx.net.openURI(linkkisivu);
+                }
+            }
+        };
+    }
 
     public void paivitaInfoButtonit(float delta, float angleToPoint, boolean zoomedOut) {
         this.zoomedOut = zoomedOut;
